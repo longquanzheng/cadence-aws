5a6761e803e2103e1f1c99996f534992a16acc54
Quanzheng Long <longer@uber.com>
2017-10-02 10:11:20 -0700

    Use cql version 3.4.4 to support cassandra 3.11

--- cadence-docker-0.3.1/start.sh
+++ cadence-docker-0.3.1/start.sh
@@ -31,7 +31,7 @@ setup_schema() {
 
 wait_for_cassandra() {
     server=`echo $CASSANDRA_SEEDS | awk -F ',' '{print $1}'`
-    until cqlsh --cqlversion=3.4.2 $server < /dev/null; do
+    until cqlsh --cqlversion=3.4.4 $server < /dev/null; do
         echo 'waiting for cassandra to start up'
         sleep 1
     done
