cb20433c922a7fd243fff050cf0370f82307baaa
Quanzheng Long <longer@uber.com>
2017-10-05 14:03:49 -0700

    set max-size for docker log

--- cmds.py
+++ cmds.py
@@ -4,11 +4,11 @@ def generate_cmd_map(application):
     install_service_cmd = ""
     if application == 'cassandra':
         #TODO --network=host is not working with Cassandra due to https://github.com/odota/core/issues/1121
-        install_service_cmd = '\'docker run  -d --name cadence-cassandra  -p 7000:7000 -p 7001:7001 -p 7199:7199 -p 9042:9042 -p 9160:9160 -e CASSANDRA_BROADCAST_ADDRESS={private_ip} -e CASSANDRA_SEEDS={cassandra_seeds} cassandra:3.11\''
+        install_service_cmd = '\'docker run  -d --name cadence-cassandra  -p 7000:7000 -p 7001:7001 -p 7199:7199 -p 9042:9042 -p 9160:9160 -e CASSANDRA_BROADCAST_ADDRESS={private_ip} -e CASSANDRA_SEEDS={cassandra_seeds} --log-opt max-size=5g cassandra:3.11\''
     elif application == 'statsd':
-        install_service_cmd = '\'docker run  -d --network=host --name cadence-statsd  -p 80-81:80-81 -p 8025-8026:8025-8026 -p 2003:2003 -p 9160:9160 kamon/grafana_graphite\''
+        install_service_cmd = '\'docker run  -d --network=host --name cadence-statsd  -p 80-81:80-81 -p 8025-8026:8025-8026 -p 2003:2003 -p 9160:9160 --log-opt max-size=5g kamon/grafana_graphite\''
     elif application in ['frontend', 'matching', 'history']:
-        install_service_cmd = '\'docker run  -d --network=host --name cadence-{application}  -e CASSANDRA_SEEDS={cassandra_seeds} -e RINGPOP_SEEDS={cadence_seeds}  -e STATSD_ENDPOINT={statsd_seeds} -e SERVICES={application}  -p 7933-7935:7933-7935   ubercadence/longer-dev:0.3.1\''
+        install_service_cmd = '\'docker run  -d --network=host --name cadence-{application}  -e CASSANDRA_SEEDS={cassandra_seeds} -e RINGPOP_SEEDS={cadence_seeds}  -e STATSD_ENDPOINT={statsd_seeds} -e SERVICES={application}  -p 7933-7935:7933-7935  --log-opt max-size=5g ubercadence/longer-dev:0.3.1\''
 
 
     cmd_map = {
