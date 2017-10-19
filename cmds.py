e95c258c172c45d05104f313f371c39dc62896cb
Quanzheng Long <longer@uber.com>
2017-10-18 17:22:39 -0700

    fix jmx

--- cmds.py
+++ cmds.py
@@ -6,7 +6,7 @@ def generate_cmd_map(application):
     params={}
     if application == 'cassandra':
         #TODO --network=host is not working with Cassandra due to https://github.com/odota/core/issues/1121
-        install_service_cmd = [ '\'docker run  -d --name cadence-cassandra  -p 7000:7000 -p 7001:7001 -p 7199:7199 -p 9042:9042 -p 9160:9160 -e CASSANDRA_BROADCAST_ADDRESS={private_ip} -e CASSANDRA_SEEDS={cassandra_seeds} --log-opt max-size=5g cassandra:3.11\'' ]
+        install_service_cmd = [ '\'docker run  -d --name cadence-cassandra  -p 7000:7000 -p 7001:7001 -p 7199:7199 -p 9042:9042 -p 9160:9160 -e LOCAL_JMX=no -e     JVM_EXTRA_OPTS=-Djava.rmi.server.hostname=127.0.0.1 -e CASSANDRA_BROADCAST_ADDRESS={private_ip} -e CASSANDRA_SEEDS={cassandra_seeds} --log-opt max-size=5g cassandra:3.11\'' ]
     elif application == 'statsd':
         install_service_cmd = ['\'docker run  -d --network=host --name cadence-statsd  -p 80-81:80-81 -p 8025-8026:8025-8026 -p 2003:2003 -p 9160:9160 --log-opt max-size=5g kamon/grafana_graphite\'']
     elif application in ['frontend', 'matching', 'history']:
@@ -73,8 +73,14 @@ def generate_cmd_map(application):
           },
 
         # install jmxtrans
+        # TODO need to figure it out how to use docker to make it easier
         'jt':{
             'cmds' : [
+                #fix jmx setting and restart cassandra
+                '\'cat > cassandra-env.sh \' < ./templates/cassandra-env.sh ',
+                'docker cp cassandra-env.sh cadence-cassandra:/etc/cassandra/cassandra-env.sh',
+                '\'docker restart cadence-cassandra \'',
+
                 # upload statsd.json
                 '\'cat > statsd_template.json \' < ./templates/statsd_template.json ',
                 'docker cp statsd_template.json cadence-cassandra:statsd_template.json',
@@ -90,7 +96,7 @@ def generate_cmd_map(application):
                 # generate statsd.json based on template TODO there is a bug about bash export command...
                 '\"docker exec -i  cadence-cassandra /bin/bash -c \\\" export STATSD_IP={statsd_seed_ip} && export PRIVATE_IP_UNDER={private_ip_under} && envsubst < statsd_template.json > /usr/share/jmxtrans/statsd.json \\\" \"',
                 # run jmxtrans # NOTE intentionally add 2s delay on the end of bash cmds to let jmx get fully started
-                '\'docker exec -i  cadence-cassandra /bin/bash -c "cd /usr/share/jmxtrans/ && ./bin/jmxtrans.sh start statsd.json && sleep 2 && echo succ"\''
+                '\'docker exec -i  cadence-cassandra /bin/bash -c "cd /usr/share/jmxtrans/ && ./bin/jmxtrans.sh stop statsd.json && ./bin/jmxtrans.sh start statsd.json && sleep 2 && echo succ"\''
             ],
             'desc':'Install jmxtrans (for Cassandra docker container)'
         },
@@ -113,7 +119,6 @@ def generate_cmd_map(application):
                 },
                 'remote_port':{
                     'default': '80',
-                    'choices': ['80', '81', '9696','9697','9698' ]
                 },
             },
             'cmds': ['-f -N -L {local_port}:{private_ip}:{remote_port}'],
