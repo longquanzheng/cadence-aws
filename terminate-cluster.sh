d43b96b7bd52c3cf73b23083f283cec4ba1224ef
Quanzheng Long <longer@uber.com>
2017-10-23 15:36:38 -0700

    minor: remove extra unused param in terminate-cluster script

--- terminate-cluster.sh
+++ terminate-cluster.sh
@@ -6,9 +6,9 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-python operate-instances.py -a statsd -d cadence-shared -d $1 -op tm --operation-params NONE --target-instances 0-100
-python operate-instances.py -a cassandra -d cadence-shared -d $1 -op tm --operation-params NONE --target-instances 0-100
-python operate-instances.py -a history -d cadence-shared -d $1 -op tm --operation-params NONE --target-instances 0-100
-python operate-instances.py -a matching -d cadence-shared -d $1 -op tm --operation-params NONE --target-instances 0-100
-python operate-instances.py -a frontend -d cadence-shared -d $1 -op tm --operation-params NONE --target-instances 0-100
-python operate-instances.py -a stress -d cadence-shared -d $1 -op tm --operation-params NONE --target-instances 0-100
+python operate-instances.py -a statsd -d $1 -op tm --operation-params NONE --target-instances 0-100
+python operate-instances.py -a cassandra -d $1 -op tm --operation-params NONE --target-instances 0-100
+python operate-instances.py -a history -d $1 -op tm --operation-params NONE --target-instances 0-100
+python operate-instances.py -a matching -d $1 -op tm --operation-params NONE --target-instances 0-100
+python operate-instances.py -a frontend -d $1 -op tm --operation-params NONE --target-instances 0-100
+python operate-instances.py -a stress -d $1 -op tm --operation-params NONE --target-instances 0-100
